fed488cb030d90bc194c499b9ec4000d10984b85
Abhishek yadav <mrabhishek1379@gmail.com>
2024-03-18 00:34:32 +0530

    Update settings.py

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=m1^$ql(%@l&m_d()&st30d!b^hc5cjl5@9!+*mlheo)v&o(y=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','127.0.0.1', '.vercel.app']
 
 
 # Application definition
