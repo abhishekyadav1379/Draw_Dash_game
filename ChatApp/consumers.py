78d1707c881a961b092264a98bfddc5083e3bedd
Abhishek yadav <mrabhishek1379@gmail.com>
2024-03-18 10:22:01 +0530

    Update consumers.py

--- ChatApp/consumers.py
+++ ChatApp/consumers.py
@@ -1,7 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from ChatApp.models import Room, Message
+# from ChatApp.models import Room, Message
 from django.shortcuts import render, redirect
 # from . import sql_code
 # from .sql_code import Sql_codes
